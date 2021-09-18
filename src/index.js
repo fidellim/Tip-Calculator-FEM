a1b043a6419cb9b0fbbaf8b7f3324070ce319110
fidellim <lim.fidel@gmail.com>
2021-09-19 00:22:48 +0400

    add service worker files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./sass/index.scss";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+// if ("serviceWorker" in navigator) {
+// 	navigator.serviceWorker
+// 		.register("sw.js")
+// 		.then((registration) => {
+// 			console.log(`SW Registered!`);
+// 			console.log(registration);
+// 		})
+// 		.catch((err) => {
+// 			console.log(`SW Registration Failed${err}`);
+// 		});
+// }
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -9,3 +22,8 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
